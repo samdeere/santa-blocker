e0110a2c3fdf74a981802a5da0c4b35b12bc343d
samdeere <sammdeere@gmail.com>
2016-11-29 15:55:27 +0000

    working on christmas checker

--- santablocker/blocker.js
+++ santablocker/blocker.js
@@ -11,6 +11,25 @@ $(document).ready(function(){
 
 function initialize(){
     console.log("initialized");
+    var text = nativeSelector();
+
+}
+
+function hasChristmasContent(){
+    //regex check on the 
+}
+
+function nativeSelector() {
+    var elements = document.querySelectorAll("body, body *"); 
+    var results = [];
+    var child;
+    for(var i = 0; i < elements.length; i++) {
+        child = elements[i].childNodes[0];
+        if(elements[i].hasChildNodes() && child.nodeType == 3) {
+            results.push(child.nodeValue);
+        }
+    }
+    return results;
 }
 
     //if on then check for christmas theme
--- santablocker/constants.js
+++ santablocker/constants.js
@@ -1,1 +1,44 @@
 var dataKey = 'santa-blocker';
+
+var christmasList = [
+    'artificial tree',
+    'bells',
+    'bough',
+    'candy cane',
+    'carolers',
+    'carols',
+    'chestnuts',
+    'christmas',
+    'december 25',
+    'eggnog',
+    'elf',
+    'elves',
+    'feliz navidad',
+    'festive',
+    'frosty',
+    'gift-giving',
+    'ice skates',
+    'joyeux noel',
+    'kris kringle',
+    'manger',
+    'mince pie',
+    'merry',
+    'mistletoe',
+    'nativity',
+    'reindeer',
+    'santa claus',
+    'sled',
+    'sleigh',
+    'snow',
+    'xmas',
+    'yule'
+]
+
+var christmasListRegex = new RegExp(christmasList.join('|'), 'i')
+
+var naughtyList = [
+    'the',
+    'and'
+]
+
+var naughtListRegex = new RegExt(naughtyList.join('|'), 'i')
