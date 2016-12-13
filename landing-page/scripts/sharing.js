cdbdbf45238262c82ce0826d95a0a77f246497cc
samdeere <sammdeere@gmail.com>
2016-12-13 12:38:20 +0000

    landing page styling

--- landing-page/scripts/sharing.js
+++ landing-page/scripts/sharing.js
@@ -2,3 +2,10 @@ function shareOnFacebook() {
     var redirectUrl = "https://www.facebook.com/sharer/sharer.php?u=blitzn.io&title=Blitzn";
     window.open(redirectUrl, "FacebookShare", "location=0,status=0,width=800,height=650");
 }
+
+function shareOnTwitter() {
+    var tweet = "Blitzn by Studio Something";
+    var url = "http://blitzn.io";
+    var twitterUrl = "https://twitter.com/intent/tweet?text="+ encodeURIComponent(tweet + " " + url) + "&related=s0methingsays";
+    window.open(twitterUrl, "Tweet", "location=0,status=0,width=800,height=650");
+}
