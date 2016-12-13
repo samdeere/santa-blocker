f183b41011c037f7d32839d903485fe9f38034f0
samdeere <sammdeere@gmail.com>
2016-12-13 11:41:27 +0000

    facebook sharing

---
+++ landing-page/scripts/sharing.js
@@ -0,0 +1,4 @@
+function shareOnFacebook() {
+    var redirectUrl = "https://www.facebook.com/sharer/sharer.php?u=blitzn.io&title=Blitzn";
+    window.open(redirectUrl, "FacebookShare", "location=0,status=0,width=800,height=650");
+}
